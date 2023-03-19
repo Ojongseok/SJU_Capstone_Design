bdb5bb42c841989b312583592dfc24675c50c39b
Ojongseok <whdtjr6889@naver.com>
2023-03-19 23:55:16 +0900

    Initial commit

---
+++ app/src/main/java/com/example/capstonedesign/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.example.capstonedesign
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.example.capstonedesign.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private val binding: ActivityMainBinding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+    private lateinit var navController: NavController
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+
+        setupJetpackNavigation()
+
+        binding.fab.bringToFront()
+    }
+
+    private fun setupJetpackNavigation() {
+        val host = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navController = host.navController
+        binding.bottomNavigationView.setupWithNavController(navController)
+
+    }
+}
