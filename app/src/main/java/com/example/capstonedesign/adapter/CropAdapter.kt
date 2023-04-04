78dc89a6972e96d010c83cddf1b32062746265c6
Ojongseok <whdtjr6889@naver.com>
2023-04-04 17:08:58 +0900

    작물 목록 이미지 List 작성

--- app/src/main/java/com/example/capstonedesign/adapter/CropAdapter.kt
+++ app/src/main/java/com/example/capstonedesign/adapter/CropAdapter.kt
@@ -1,39 +1,28 @@
 package com.example.capstonedesign.adapter
 
 import android.content.Context
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.lifecycle.MutableLiveData
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.example.capstonedesign.databinding.ItemCropBinding
-import com.example.capstonedesign.model.PostTest
-import com.example.capstonedesign.databinding.ItemRequestPostBinding
-import com.example.capstonedesign.viewmodel.OpenApiViewModel
 import org.jsoup.nodes.Element
-import org.jsoup.select.Elements
 
-class CropAdapter(private val context: Context) : RecyclerView.Adapter<CropAdapter.CustomViewHolder>() {
+class CropAdapter(private val context: Context, private val cropImgList: List<String>) : RecyclerView.Adapter<CropAdapter.CustomViewHolder>() {
     private lateinit var itemClickListener: OnItemClickListener
     private val url = "https://ncpms.rda.go.kr/npms/"
     private var cropList = listOf<Element>()
 
     inner class CustomViewHolder(private val binding: ItemCropBinding): RecyclerView.ViewHolder(binding.root) {
-        fun bind(item: Element) {
-            if (position == 1) {
-                Log.d("tag",url + item.select("img.imgBord").attr("src"))
-//                Log.d("tag",item.select("img.imgBord").attr("alt"))
-//                Log.d("tag",item.select("img.imgBord").toString())
-            }
-            Glide.with(context).load(url + item.select("img.imgBord").attr("src")).into(binding.ivItemCrop)
+        fun bind(item: Element, img: String) {
+            Glide.with(context).load(img).into(binding.ivItemCrop)
             binding.tvItemCropName.text = item.select("img.imgBord").attr("alt").toString()
         }
     }
 
     override fun onBindViewHolder(holder: CustomViewHolder, position: Int) {
-        holder.bind(cropList[position])
+        holder.bind(cropList[position], cropImgList[position])
 
         holder.itemView.setOnClickListener {
             itemClickListener.onClick(it,position)
