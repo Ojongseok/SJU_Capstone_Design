08d6babe2fe83525ae14eac44c31c3638d963613
Ojongseok <whdtjr6889@naver.com>
2023-04-16 15:55:49 +0900

    [Gitflow] 설정 전

---
+++ app/src/main/java/com/example/capstonedesign/model/board/PostDetailInfoResponse.kt
@@ -0,0 +1,5 @@
+package com.example.capstonedesign.model.board
+
+data class PostDetailInfoResponse(
+    val s: String
+)
--- app/src/main/java/com/example/capstonedesign/viewmodel/BoardViewModel.kt
+++ app/src/main/java/com/example/capstonedesign/viewmodel/BoardViewModel.kt
@@ -4,6 +4,7 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.capstonedesign.model.board.AllPostResponse
+import com.example.capstonedesign.model.board.PostDetailInfoResponse
 import com.example.capstonedesign.repository.BoardRepository
 import kotlinx.coroutines.launch
 import okhttp3.MultipartBody
@@ -12,6 +13,7 @@ import okhttp3.RequestBody
 class BoardViewModel(private val repository: BoardRepository): ViewModel() {
     val writePostResultCode = MutableLiveData<Int>()    // 게시글 작성 결과
     val allPost = MutableLiveData<AllPostResponse>()    // 게시글 전체 조회
+    val postDetailInfoResponse = MutableLiveData<PostDetailInfoResponse>()    // 게시글 상제 정보
 
     // 게시글 작성
     fun writePost(jsonBody: RequestBody, file: MultipartBody.Part?) = viewModelScope.launch {
